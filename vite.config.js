5347acabe1910d40f1d6dc706fa959a9d2aa0c9d
Mansur <mansur@mail.com>
2026-08-27 17:50:21 +0300

    build: revert to default export for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [react()],
   base: '/circles-and-sections/',
 });
-
-export { config };
